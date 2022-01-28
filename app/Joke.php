65b05e2cd3da4dc606b640152be866188081192a
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-01-28 16:14:21 +0100

    ADD: method to create and pagination

--- app/Joke.php
+++ app/Joke.php
@@ -6,4 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Joke extends Model
 {
+    protected $fillable = ['setup', 'punchline'];
 }
--- resources/views/jokes/index.blade.php
+++ resources/views/jokes/index.blade.php
@@ -14,6 +14,7 @@
                         </div>
                         @endforeach
                     </div>
+                    {{ $jokes->links() }}
                 </div>
             </div>
         </div>
